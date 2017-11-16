c3d02ffb7b8d58042b479fcb96766afda0871425
Lukasz Lacinski <lukasz@uchicago.edu>
2017-11-16 09:02:06 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # esgf-auth
-ESGF OAuth2 client
+OpenID/OAuth2 authentication client for ESGF
+
+The ESGF authentication client is a Django web aplication that replaces the 
+ESGF ORP. If a dataset file requires authentication, the THREDDS 
+authentication filter redirects a user web browser to the web app. The web
+app discovers authentication services provided by an IdP selected by a user
+in the drop down, and redirects the user web browser to OAuth2 or OpenID
+server where the user enters his/her username and password. When the user 
+is authenticated successfully, the OAuth2 (or OpenID) server redirects the 
+user web browser back to the web app. The web app sets a secret cookie and
+redirects the user back to the THREDDS authentication filter.
+
+# Install esgf-auth
+
+Create Python 2.7 virtual environment
+```
+$ python --version 
+Python 2.7.10
+$ virtualenv venv
+$ . venv/bin/activate
+```
+Download and install crypto-cookie:
+```
+(venv)$ git clone git@github.com:philipkershaw/crypto-cookie
+(venv)$ cd crypto-cookie
+(venv)$ python setup.py
+(venv)$ cd ..
+```
+Download and install esgf-auth with dependencies (Django, 
+social-auth-app-django, social-auth-core, etc.)
+```
+(venv)$ git clone git@github.com:lukaszlacinski/esgf-auth
+(venv)$ cd esgf-auth
+(venv)$ pip install -r requirements.txt
+```
+Create the database
+```
+(venv)$ ./manage.py migrate
+```
+Create /esg/config/.esgf_oauth2.json file with a client key and secret 
+received from an admin of an ESGF OAuth2 server. When you register your 
+OAuth2 client, your redirect URI is 
+`https://<your_hostname>/esgf-auth/complete/esgf/`. Here is a sample
+esgf_oauth2.json file:
+```
+{
+    "ceda.ac.uk":
+        "key": "BUwBQaqS7qs2pSLhwHiAQlqt+hc=",
+        "secret": "Qf+EsAoDmZzdW1L/H4zAj2u/tg3ISCnqxby+2bD7hY/GCZcRJgUjFQ=="
+    },
+    "esgf-node.llnl.gov": {
+        "key": "RMRXIub0/m4RIfo7sdr2OiGOTmc=",
+        "secret": "N9PT+3/rGnjGPkEBsyzhoggsyFYdX6ptPG9Gy6Olb0j8ub/4+DJtiA=="
+    }
+}
+```
+
+Create a second file /esg/config/esgf_auth_config.json with additional site-specific information. An example of such configuration file is:
+```
+{
+    "ESGF_HOSTNAME":"my-node.esgf.org",
+    "ESGF_SECRET_KEY":"o7GIieXmQzUGslstjWS7d8==",
+    "DJANGO_SECRET_KEY":"xu0jf]LBUHfLHWbHXzBcDuffHaWQYrev8ojXXd1M",
+    "OAUTH@_SECRET_FILE: "/esg/config/.esgf_oauth2.json"
+}
+```
+where all values need to be changed by each site administrator. The value of "ESGF_SECRET_KEY" must match the value used to encode the authentication cookies that is configured in the THREDDS web.xml file.
+
+
+# Apache/mod_wsgi
+
+For example, on Ubuntu, add the following lines to 
+/etc/apache2/sites-available/default-ssl,conf in 
+`<VirtualHost _default_:443>`
+
+```
+    WSGIDaemonProcess esgf_auth python-path=<your_base_dir>/esgf-auth:<your_base_dir>/venv/lib/python2.7/site-packages
+    WSGIScriptAlias /esgf-auth <your_base_dir>/esgf-auth/esgf_auth/wsgi.py process-group=esgf_auth
+    <Directory <your_base_dir>/esgf-auth/esgf_auth>
+        <Files wsgi.py>
+            # Apache >= 2.4
+            #Require all granted
+            # Apache <= 2.2
+            Order allow,deny
+            Allow from all
+        </Files>
+    </Directory>
+    Alias /esgf-auth/home/static/ <your_base_dir>/esgf-auth/static/
+    <Directory <your_base_dir>/esgf-auth/static>
+        Options -Indexes
+        # Apache >= 2.4
+        #Require all granted
+        # Apache <= 2.2
+        Order allow,deny
+        Allow from all
+        AllowOverride None
+    </Directory>
+```
+After restarting Apache, open `https://<your_hostname>/esgf-auth/thredds/` 
+in a web browser. The page mimics THREDDS server with the authentication 
+filter. You will also likely need to change ownership of the 'esgf-auth' directory to 
+www-data (on Ubuntu), so Apache can access the SQLite3 database file.
---
+++ esgf_auth/views.py
@@ -0,0 +1,193 @@
+import os
+import json
+import logging
+import traceback
+from xml.dom import minidom
+from urlparse import urlparse
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from social_django.utils import load_strategy
+from backends.esgf import discover
+from urllib import quote
+from crypto_cookie.encoding import Encoder
+from crypto_cookie.auth_tkt import SecureCookie
+
+
+log = logging.getLogger(__name__)
+
+
+'''
+Parse /esg/config/esgf_known_providers.xml to display a dropdown list
+with the ESGF IdP nodes.
+'''
+def get_known_providers():
+    known_providers = []
+
+    try:
+        r = minidom.parse(settings.ESGF_KNOWN_PROVIDERS)
+    except Exception:
+        traceback.print_exc()
+        return known_providers
+
+    for op in r.getElementsByTagName('OP'):
+        try:
+            name = op.getElementsByTagName('NAME')[0].childNodes[0].data
+            url = op.getElementsByTagName('URL')[0].childNodes[0].data
+            known_providers.append({'name': name, 'url': url})
+        except Exception:
+            pass
+
+    return known_providers
+
+
+'''
+Get a key and secret pair from /esg/config/esgf_oauth2.json
+'''
+def get_oauth2_cred(openid_identifier):
+    parsed_openid = urlparse(openid_identifier)
+    with open(settings.ESGF_OAUTH2_SECRET_FILE, 'r') as f:
+        try:
+            creds = json.loads(f.read())
+            cred = creds.get(parsed_openid.netloc)
+            if len(cred['key']) and len(cred['secret']):
+                return cred
+        except Exception:
+            traceback.print_exc()
+    return None
+
+
+'''
+The home view corresponds to /esg-orp/home.htm, however it supports both OpenID
+and OAuth2.
+'''
+def home(request):
+    if request.method == 'GET':
+        '''
+        requested directly or
+        redirected from THREDDS/authentication filter or
+        redirected from OpenID/OAuth2 server
+        '''
+        if request.user.is_authenticated():
+            social = None
+            try:
+                social = request.user.social_auth.get(provider='esgf')
+            except Exception:
+                pass
+
+            try:
+                social = request.user.social_auth.get(provider='esgf-openid')
+            except Exception:
+                pass
+
+            log.info('User {} successfully authenticated'.format(social.uid))
+
+            redirect_url = request.GET.get('redirect', request.session.get('redirect'))
+
+            if not redirect_url:
+                '''
+                not redirected from THREDDS/authentication filter
+                '''
+                print '1', get_known_providers()
+                return render(request,
+                              'auth/home.j2',
+                              {'openid_identifier': social.uid,
+                              'known_providers': get_known_providers()})
+            '''
+            redirected from OpenID/OAuth2 server or
+            redirected from THREDDS/authentication filter
+            '''
+            # create a session cookie
+            encoded_secret_key = settings.ESGF_SECRET_KEY
+            session_cookie_name = settings.ESGF_SESSION_COOKIE_NAME
+            secret_key = encoded_secret_key.decode('base64')
+            secure_cookie = SecureCookie(secret_key, social.uid, '127.0.0.1', (), '')
+
+            # redirect back to THREDDS
+            response = redirect(redirect_url)
+            response.set_cookie(session_cookie_name, secure_cookie.cookie_value())
+            return response
+
+        else:
+            '''
+            requested directly or
+            redirected from THREDDS/authentication filter
+            '''
+            redirect_url = request.GET.get('redirect', None)
+            '''
+            Save 'redirect' param passed in the redirection by the THREDDS
+            authentication filter before starting the OAuth2 flow. After the
+            OAuth2 flow is completed, the param will be needed to create a
+            redirection back to THREDDS.
+            '''
+            request.session['redirect'] = redirect_url
+            print '2', get_known_providers()
+            return render(request,
+                          'auth/home.j2',
+                          {'redirect': redirect_url,
+                          'known_providers': get_known_providers()})
+
+    # request.method == 'POST'
+    openid_identifier = request.POST.get('openid_identifier')
+    request.session['openid_identifier'] = openid_identifier
+    protocol = discover(openid_identifier)
+    if protocol:
+        request.session['next'] = request.path
+        credential = get_oauth2_cred(openid_identifier)
+        if protocol == 'OAuth2' and credential:
+            '''
+            It may create a race condition.  When two users authenticate at the
+            same moment  through different OAuth2 servers, KEY and SECRET may
+            be set for a wrong server. It will be better to set KEY and SECRET
+            directly on a strategy object.
+            '''
+            settings.SOCIAL_AUTH_ESGF_KEY = credential['key']
+            settings.SOCIAL_AUTH_ESGF_SECRET = credential['secret']
+            return redirect(reverse('social:begin', args=['esgf']))
+        elif protocol == 'OpenID':
+            return redirect(reverse('social:begin', args=['esgf-openid']))
+    else:
+        log.error('Could not discover authentication service for {}'.format(openid_identifier))
+        print '3', get_known_providers()
+        return render(request,
+                      'auth/home.j2',
+                      {'redirect': redirect_url,
+                      'known_providers': get_known_providers(),
+                      'message': 'No OpenID/OAuth2/OIDC service discovered'})
+
+
+'''
+A test view to mimic THREDDS/authentication filter. On ESGF systems with
+THREDDS deployed, Apache is configured as a reverse proxy for the '/thredds'
+path and this view is not be accessible.
+'''
+def thredds(request):
+    encoded_secret_key = settings.ESGF_SECRET_KEY
+    return_query_name = settings.ESGF_RETURN_QUERY_NAME
+    session_cookie_name = settings.ESGF_SESSION_COOKIE_NAME
+
+    if request.is_secure():
+        scheme = 'https'
+    else:
+        scheme = 'http'
+    authenticate_url = '{}://{}{}'.format(scheme, request.get_host(), reverse('home'))
+    redirect = '{}://{}{}{}'.format(scheme, request.get_host(), reverse('thredds'), 'fileServer/cmip5_css02_data/cmip5/output1/CMCC/CMCC-CM/historical/day/atmos/day/r1i1p1/clt/1/clt_day_CMCC-CM_historical_r1i1p1_20050101-20051231.nc')
+
+    session_cookie = request.COOKIES.get(session_cookie_name)
+    cookie = None
+    if session_cookie:
+        secret_key = encoded_secret_key.decode('base64')
+        cookie = SecureCookie.parse_ticket(secret_key, session_cookie, None, None)
+
+    return render(request,
+            'auth/thredds.j2', {
+                'authenticate_url': authenticate_url,
+                'return_query_name': return_query_name,
+                'encoded_secret_key': encoded_secret_key,
+                'quoted_encoded_secret_key': quote(encoded_secret_key),
+                'session_cookie_name': session_cookie_name,
+                'redirect': redirect,
+                'session_cookie': session_cookie,
+                'decrypted_session_cookie': cookie
+            })
+
